af6224ab7b5d4ea3b36711d14612858a32806a3c
Srinivas Dasari <sodhanaware@gmail.com>
2024-11-10 17:30:35 +0530

    Release

---
+++ tests/test.js
@@ -0,0 +1,7 @@
+// // test.js
+// const { toUpperCase, reverseString, isPalindrome } = require('./src/index');
+
+// console.log(toUpperCase("hello world")); // Output: HELLO WORLD
+// console.log(reverseString("hello world")); // Output: dlrow olleh
+// console.log(isPalindrome("madam")); // Output: true
+// console.log(isPalindrome("hello")); // Output: false
